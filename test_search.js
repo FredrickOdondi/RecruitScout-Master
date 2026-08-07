325d5cfe4d7fa9ddce14d38b702a9d3216456519
FredrickOdondi <fredrickodondi@users.noreply.github.com>
2026-08-07 23:02:03 +0300

    chore: remove test file

--- test_search.js
+++
@@ -1,8 +0,0 @@
-const recruiters = [{"Country":"Italy","Location":"Milano, Lombardia","Name":"APraise","Size":"11-50 employees","Domain":"apraise.it","status":"Active"}];
-const searchQuery = "apr";
-const filteredRecruiters = recruiters.filter(r => {
-  if (!searchQuery) return true;
-  const name = r['Name'] || r['Agency Name'] || '';
-  return name.toLowerCase().includes(searchQuery.toLowerCase());
-});
-console.log(filteredRecruiters);
